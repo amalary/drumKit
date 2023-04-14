3c7e9ea4ae73cf8eae680fd9b3bea6dae5e6a938
Anthony Malary <amalary@bu.edu>
2023-04-14 11:43:44 -0700

    Getting sound from key strokes

--- script/app.js
+++ script/app.js
@@ -1,8 +1,10 @@
 function playSound(e){
   const audio = document.querySelector(`audio[data-key= "${e.keyCode}"]`)
-  const key = document.querySelector(`dic[data-key= "${e.keyCode}"]`)
+  const key = document.querySelector(`div[data-key= "${e.keyCode}"]`)
   if(!audio) return; 
-  key.classList.add('playing')
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
 }
 
 window.addEventListener("keydown", playSound)
